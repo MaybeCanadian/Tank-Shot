2fcd5a0adef2160e94f13e949a32c025a1e1b681
MaybeCanadian <1coffeye@gmail.com>
2023-05-02 06:35:07 -0400

    remade the tank controller.

--- Assets/Scripts/Tank/TankController.cs
+++ Assets/Scripts/Tank/TankController.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class TankController : MonoBehaviour
 {
     [Header("Visuals")]
-    public TankVisuals visuals;
+    public TankVisuals visuals = null;
+
+    [Header("Rotation")]
+    public float rotation = 0.0f;
+    public float rotateSpeed = 50.0f;
+
+    [Header("Movement")]
+    public float moveSpeed = 1.0f;
+
+    private Vector2 moveInput = Vector2.zero;
 
     private void Awake()
     {
@@ -17,5 +28,40 @@ public class TankController : MonoBehaviour
         }
     }
 
+    #region Movement
+    private void FixedUpdate()
+    {
+        Rotate();
 
+        Move();
+    }
+    private void Rotate()
+    {
+        rotation += rotateSpeed * moveInput.x * Time.fixedDeltaTime;
+
+        if(rotation < 0)
+        {
+            rotation += 360;
+        }
+        else if(rotation > 360)
+        {
+            rotation = rotation % 360;
+        }
+
+        visuals.SetBodyRotation(rotation);
+    }
+    private void Move()
+    {
+        Vector3 forwards = new Vector3(Mathf.Cos(rotation * Mathf.Deg2Rad), -Mathf.Sin(rotation * Mathf.Deg2Rad), 0.0f).normalized;
+
+        transform.position += forwards * moveSpeed * Time.fixedDeltaTime;
+    }
+    #endregion
+
+    #region Event Recievers
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+    #endregion
 }
